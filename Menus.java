e2d2bda4b4898d9fb6354b5ae43cdfb9a17e862b
Vinicius-R-de-L <91351819+Vinicius-R-de-L@users.noreply.github.com>
2022-02-02 20:50:51 -0300

    Add files via upload

---
+++ Menus.java
@@ -0,0 +1,71 @@
+public class Menus{
+	public static void printMenu(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t  Player de Video/Musica");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Selecionar Midia");
+		System.out.println("\n 2) Ver Midia(s)");
+		System.out.println("\n 3) Adicionar Midia");
+		System.out.println("\n 4) Configuracoes");
+		System.out.println("\n 5) Dispositivos");
+		System.out.println("\n 6) Usuario");
+		System.out.println("\n 7) Sair");
+		System.out.print("\n\n>>> ");
+	}
+	
+	public static void printMenuOp1(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t     Selecionar Midia");
+		System.out.println("\t--------------------------\n\n");
+	}
+	
+	public static void printMenuOp2(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t\t  Midias");
+		System.out.println("\t--------------------------\n\n");
+	}
+	
+	public static void printMenuOp3(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t  Adicionar Video/Musica");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Adicionar Musica");
+		System.out.println("\n 2) Adicionar Video");
+		System.out.println("\n 3) Outro tipo de Midia");
+		System.out.println("\n 4) Voltar");
+		System.out.print("\n\n>>> ");
+	}
+	
+	public static void printMenuOp4(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t      Configuracoes");
+		System.out.println("\t--------------------------");
+		System.out.print("\n 1) Velocidade de reproducao");
+		System.out.print("\n 2) Qualidade de reproducao");
+		System.out.print("\n 3) Estilo");
+		System.out.print("\n 4) Voltar");
+		System.out.print("\n\n>>> ");
+	}
+	
+	public static void printMenuOp5(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t      Dispositivos");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Adicionar Dispositivo");
+		System.out.println("\n 2) Remover Dispositivo");
+		System.out.println("\n 3) Ver Dispositivos");
+		System.out.print("\n\n>>> ");
+	}
+
+	public static void printMenuOp6(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t      Usuario");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Cadastrar Usuario com Nome");
+		System.out.println("\n 2) Cadastrar Usuario com Nome e Endereco");
+		System.out.println("\n 3) Cadastrar Usuario com Nome, Endereco e Idade");
+		System.out.println("\n 4) Cadastrar Usuario com Nome e Idade");
+		System.out.println("\n 5) Ver Informacoes do Usuario");
+		System.out.print("\n\n>>> ");
+	}
+}
